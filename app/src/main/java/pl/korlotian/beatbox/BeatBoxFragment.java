be0bdbc2d4c1f574133b233c5afb9380665ca602
Maria Stokłosa <maria.stoklosa@netguru.pl>
2017-01-29 20:08:09 +0100

    Create SoundHolder and SoundAdapter

--- app/src/main/java/pl/korlotian/beatbox/BeatBoxFragment.java
+++ app/src/main/java/pl/korlotian/beatbox/BeatBoxFragment.java
@@ -7,6 +7,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class BeatBoxFragment extends Fragment {
     public static BeatBoxFragment newInstance() {
@@ -23,5 +24,34 @@ public class BeatBoxFragment extends Fragment {
         recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
 
         return view;
+    }
+
+    private class SoundHolder extends RecyclerView.ViewHolder {
+        private Button mButton;
+
+        public SoundHolder(LayoutInflater inflater, ViewGroup container) {
+            super(inflater.inflate(R.layout.list_item_sound, container, false));
+
+            mButton = (Button) itemView.findViewById(R.id.list_item_sound_button);
+        }
+    }
+
+    private class SoundAdapter extends RecyclerView.Adapter<SoundHolder> {
+
+        @Override
+        public SoundHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater inflater = LayoutInflater.from(getActivity());
+            return new SoundHolder(inflater, parent);
+        }
+
+        @Override
+        public void onBindViewHolder(SoundHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 0;
+        }
     }
 }
