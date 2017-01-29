dd198bdceb22420132e8b4a3efdfb2f7e56f58b7
Maria Stokłosa <maria.stoklosa@netguru.pl>
2017-01-29 20:12:32 +0100

    Use single fragment activity in app

--- app/src/main/java/pl/korlotian/beatbox/BeatBoxFragment.java
+++ app/src/main/java/pl/korlotian/beatbox/BeatBoxFragment.java
@@ -22,6 +22,7 @@ public class BeatBoxFragment extends Fragment {
         RecyclerView recyclerView = (RecyclerView) view
                 .findViewById(R.id.fragment_beat_box_recycler_view);
         recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
+        recyclerView.setAdapter(new SoundAdapter());
 
         return view;
     }
