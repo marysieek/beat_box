07cbee6b118b4eca63828314864a07a0fe001084
Maria Korlotian <maria.stoklosa@netguru.pl>
2017-03-11 20:17:07 +0100

    Load sounds

--- app/src/main/java/pl/korlotian/beatbox/Sound.java
+++ app/src/main/java/pl/korlotian/beatbox/Sound.java
@@ -3,12 +3,21 @@ package pl.korlotian.beatbox;
 public class Sound {
     private String mAssetPath;
     private String mName;
+    private Integer mSoundId;
 
     public Sound(String assetPath) {
         mAssetPath = assetPath;
         String[] components = assetPath.split("/");
         String filename = components[components.length - 1];
         mName = filename.replace(".wav", "");
+    }
+
+    public Integer getSoundId() {
+        return mSoundId;
+    }
+
+    public void setSoundId(Integer soundId) {
+        mSoundId = soundId;
     }
 
     public String getAssetPath() {
